cdba4ac54299664b31b28c4776272c7d486216ff
WarLikeLaux <warlikelaux080399@mail.ru>
2020-03-13 13:43:23 +0600

    Change the font-size, size of logo and add adaption for mobile devices

--- index.php
+++ index.php
@@ -17,17 +17,18 @@
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
   <link rel="stylesheet" href="css/style.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <title>Я в профессии</title>
 </head>
 
 <body>
   <nav class="navbar navbar-expand-lg navbar-ligh">
     <div class="container">
       <div>
-        <h4>Я в профессии</h1>
+        <h3>Я в профессии</h3>
       </div>
       <div>
         <a class="navbar-brand text-dark">
-          <img height="30px" src="img/logo.png">
+          <img class="logo" src="img/logo.png">
         </a>
       </div>
     </div>
